860f484ea9989d79485ff0c9786171fb9ac5bea2
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2022-01-01 15:35:29 +0330

    feat: add NonPreemptiveSJF algorithm

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,2 +1,3 @@
 from algorithms.fifo import FIFO
 from algorithms.priority_p import PriorityPreemptive
+from algorithms.np_sjf import NonPreemptiveSJF
---
+++ algorithms/np_sjf.py
@@ -0,0 +1,76 @@
+from algorithms.base_algorithm import BaseAlgorithm
+
+
+class NonPreemptiveSJF(BaseAlgorithm):
+    def __init__(self, processes):
+        super().__init__(processes)
+        self.time = 0.0
+        self.idle_time = 0.0
+
+    def run(self):
+        """
+        Schedule the processes using Non-Preemptive Shortest Job First
+        algorithm and calculate following information.
+        :return: {
+            "processes": list of executed processes,
+            "time": total time of execution,
+            "cpu_utilization": total CPU utilization,
+            "throughput": total throughput,
+            "average_waiting_time": average waiting time,
+            "average_turnaround_time": average turnaround time,
+            "average_response_time": average response time
+        }
+        """
+        executed_processes = []
+        while self.processes:
+            # Get the process with the shortest remaining time
+            shortest_process = self.get_shortest_process()
+            if shortest_process:
+                # Execute the process
+                shortest_process.start_time = self.time
+                self.time += shortest_process.burst_time
+                shortest_process.end_time = self.time
+                shortest_process.turnaround_time = shortest_process.end_time - shortest_process.arrival_time
+                shortest_process.waiting_time = shortest_process.turnaround_time - shortest_process.burst_time
+                self.processes.remove(shortest_process)
+                executed_processes.append(shortest_process)
+            else:
+                # Idle time
+                self.idle_time += 1
+                self.time += 1
+
+        try:
+            avg_waiting_time = sum([process.waiting_time for process in executed_processes]) / len(executed_processes)
+        except ZeroDivisionError:
+            avg_waiting_time = 0
+
+        try:
+            avg_turnaround_time = sum([process.turnaround_time for process in executed_processes]) / len(
+                executed_processes)
+        except ZeroDivisionError:
+            avg_turnaround_time = 0
+
+        try:
+            avg_response_time = sum([process.start_time for process in executed_processes]) / len(executed_processes)
+        except ZeroDivisionError:
+            avg_response_time = 0
+
+        return {
+            "processes": executed_processes,
+            "total_time": self.time,
+            "cpu_utilization": (self.time - self.idle_time) / self.time,
+            "throughput": len(executed_processes) / self.time,
+            "average_waiting_time": avg_waiting_time,
+            "average_turnaround_time": avg_turnaround_time,
+            "average_response_time": avg_response_time
+        }
+
+    def get_shortest_process(self):
+        """
+        Get the process with the shortest remaining time among arrive processes.
+        :return: Process
+        """
+        processes = [process for process in self.processes if process.arrival_time <= self.time]
+        if processes:
+            return min(processes, key=lambda process: process.burst_time)
+        return None
