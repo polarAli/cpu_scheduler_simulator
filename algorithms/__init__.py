4518ba1e1a4079e36cde7a0d0438a269f2f4ec83
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2022-01-05 19:23:00 +0330

    feat: add `NonPreemptivePriority` algorithm

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,3 +1,4 @@
 from algorithms.fifo import FIFO
 from algorithms.priority_p import PriorityPreemptive
 from algorithms.np_sjf import NonPreemptiveSJF
+from algorithms.np_priority import NonPreemptivePriority
