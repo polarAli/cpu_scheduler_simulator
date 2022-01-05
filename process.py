c947f003acbee8196ed501690fbeaea5c793af59
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2022-01-05 18:06:59 +0330

    feat: add `compare_prop` to Process and implement `__lt__` by it

--- process.py
+++ process.py
@@ -10,7 +10,7 @@ class Process:
     Process class for the scheduler simulation.
     """
 
-    def __init__(self, pid, arrival_time, priority, burst_time):
+    def __init__(self, pid, arrival_time, priority, burst_time, compare_prop='priority'):
         """
         Initialize a process.
         """
@@ -25,6 +25,7 @@ class Process:
         self.turnaround_time = 0
         self.waiting_time = 0
         self.state = State.READY
+        self.compare_prop = compare_prop
 
     @property
     def response_time(self):
@@ -40,3 +41,6 @@ class Process:
         return "pid: {}, arrival_time: {}, priority: {}, remaining_time: {}".format(
             self.pid, self.arrival_time, self.priority, self.remaining_time
         )
+
+    def __lt__(self, other):
+        return getattr(self, self.compare_prop) < getattr(other, self.compare_prop)
