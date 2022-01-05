82a585b80916490787a807089203967456d03250
AliGhotbizadeh <ali.ghotbizadeh@gmail.com>
2022-01-05 19:35:40 +0330

    feat: add `PreemptiveSJF` algorithm

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,3 +2,4 @@ from algorithms.fifo import FIFO
 from algorithms.priority_p import PriorityPreemptive
 from algorithms.np_sjf import NonPreemptiveSJF
 from algorithms.np_priority import NonPreemptivePriority
+from algorithms.sjf import PreemptiveSJF
